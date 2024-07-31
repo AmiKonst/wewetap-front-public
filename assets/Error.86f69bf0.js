2c7ddd3d1d42f34fb14070268eba8828efd5cb68
Konstantin Tyufyakin <k.tyufyakin@marine.tech>
2024-07-31 18:39:14 +0300

    weifth weweild

---
+++ assets/Error.86f69bf0.js
@@ -0,0 +1,31 @@
+import { E as EmptyLabel } from './EmptyLabel.e179ad9c.js';
+import { _ as _export_sfc, u as useI18n, a as resolveComponent, o as openBlock, b as createElementBlock, e as createVNode, s as unref } from './index.c9ec0073.js';
+
+const Error_vue_vue_type_style_index_0_scoped_207d491d_lang = '';
+
+const _hoisted_1 = { class: "page-container" };
+
+    
+const _sfc_main = {
+  __name: 'Error',
+  setup(__props) {
+
+    const { t } = useI18n();
+
+return (_ctx, _cache) => {
+  const _component_Icon = resolveComponent("Icon");
+
+  return (openBlock(), createElementBlock("div", _hoisted_1, [
+    createVNode(_component_Icon, { icon: "loki" }),
+    createVNode(EmptyLabel, {
+      title: unref(t)('pages.error.title'),
+      description: unref(t)('pages.error.description')
+    }, null, 8, ["title", "description"])
+  ]))
+}
+}
+
+};
+const Error = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-207d491d"]]);
+
+export { Error as default };
---
+++ assets/webworkerAll.7b9f7d1a.js
@@ -0,0 +1,4 @@
+import './init.bce0cef9.js';
+import './index.c9ec0073.js';
+import './colorToUniform.5ad15141.js';
+import './getBatchSamplersUniformGroup.e880daa7.js';
