badd3ee6a4dc308b125baae74fcc57a79847c46e
Konstantin Tyufyakin <k.tyufyakin@marine.tech>
2024-08-01 14:24:42 +0300

    build 01.08

---
+++ assets/Wallet.4e50a4b7.js
@@ -0,0 +1,10 @@
+import { _ as _export_sfc } from './index.8d76c780.js';
+
+const _sfc_main = {};
+
+function _sfc_render(_ctx, _cache) {
+  return "wallet"
+}
+const Wallet = /*#__PURE__*/_export_sfc(_sfc_main, [['render',_sfc_render]]);
+
+export { Wallet as default };
---
+++ assets/webworkerAll.8f9ebc9f.js
@@ -0,0 +1,4 @@
+import './init.9acdb86a.js';
+import './index.8d76c780.js';
+import './colorToUniform.2dbc8f94.js';
+import './getBatchSamplersUniformGroup.a2a98a44.js';
