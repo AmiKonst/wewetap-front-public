dd297b49eb1bf7c24502d0281019353e29fdd2bf
Konstantin Tyufyakin <k.tyufyakin@marine.tech>
2024-08-01 18:32:29 +0300

    build 01.08

---
+++ assets/Avatar.9b8eff56.js
@@ -0,0 +1,22 @@
+import { _ as _export_sfc, o as openBlock, b as createElementBlock, t as toDisplayString } from './index.1aeacb58.js';
+
+const Avatar_vue_vue_type_style_index_0_scoped_a429b24b_lang = '';
+
+const _sfc_main = {
+  __name: 'Avatar',
+  props: {
+    name: { type: String, default: '' },
+},
+  setup(__props) {
+
+const props = __props;
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock("span", null, toDisplayString(props.name?.length ? props.name[0] : '-'), 1))
+}
+}
+
+};
+const Avatar = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-a429b24b"]]);
+
+export { Avatar as A };
---
+++ assets/webworkerAll.d6fbfc4c.js
@@ -0,0 +1,4 @@
+import './init.90781b19.js';
+import './index.1aeacb58.js';
+import './colorToUniform.1ea31b69.js';
+import './getBatchSamplersUniformGroup.302969a9.js';
